b5a55a6063c1030e88305e43013fc8e5dbf334e7
Ahmed Zafar <ahmed.zafar@outlook.com>
2025-01-30 17:20:31 -0700

    Created Shape class

---
+++ app/src/main/java/com/example/codegrafters/Shape.java
@@ -0,0 +1,27 @@
+package com.example.codegrafters;
+
+public class Shape {
+    int x;
+    int y;
+
+    public Shape(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
